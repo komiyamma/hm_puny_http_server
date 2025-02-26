cc8a1ae9f9fab287e9292204b8096f49303df75a
komiyamma <komiyama0629@gmail.com>
2025-02-26 11:17:45 +0900

    呼び出し元の秀丸が自然クローズではなく、エラーやタスクマネージャから強制的に終了させられたとしても、終わるようにした。

---
+++ src/HmPunyHttpServer/hidemaru_wnd.cpp
@@ -0,0 +1,3 @@
+#include <windows.h>
+
+HWND hidemaruWnd = NULL;
--- src/HmPunyHttpServer/main.cpp
+++ src/HmPunyHttpServer/main.cpp
@@ -1,7 +1,7 @@
 ﻿#include "mongoose.h"   // To build, run: cc main.c mongoose.c
 #include <iostream>
 
-
+#include <string>
 
 // HTTP server event handler function
 void ev_handler(struct mg_connection* c, int ev, void* ev_data) {
@@ -25,13 +25,20 @@ BOOL WINAPI ConsoleCtrlHandler(DWORD ctrlType) {
     }
 }
 
-int getAvailablePort();
+extern HWND hidemaruWnd;
+extern int getAvailablePort();
 
 int main(int argc, char* argv[]) {
     using namespace std;
 
     // コンソール制御ハンドラを設定
     SetConsoleCtrlHandler(ConsoleCtrlHandler, TRUE);
+
+    if (argc >= 2) {
+        // 一番目の引数を、数値文字列は秀丸のウィンドウハンドル
+        hidemaruWnd = (HWND)(std::stoul(argv[1], nullptr));
+    }
+
 
     int port = getAvailablePort();
     if (port <= 0) {
@@ -55,6 +62,13 @@ int main(int argc, char* argv[]) {
     cout << port << endl; // flush兼ねる
 
     for (;;) {          // Run an infinite event loop
+
+        // 有効な値を引数としてもらっているのに、そらがウィンドウでないなら終了
+        // 秀丸から起動して、該当の秀丸を閉じたか、もしくは秀丸が強制終了してしまった可能性
+		if (hidemaruWnd && !IsWindow(hidemaruWnd)) {
+			break;
+		}
+
         mg_mgr_poll(&mgr, 1000);
     }
     return 0;
